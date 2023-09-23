e5a8834b9286326746ccacc049b9de1f8a0bc8c2
karlronsaria <andrew.mikedaniels@gmail.com>
2023-09-22 20:14:43 -0700

    fix(Timer): Start-Timer beep sound

--- script/Timer.ps1
+++ script/Timer.ps1
@@ -1,3 +1,8 @@
+<#
+.LINK
+Url: https://superuser.com/questions/1623573/powershell-how-to-play-different-system-sounds
+Retrieved: 2023_09_22
+#>
 function Start-Timer {
     Param(
         [Int]
@@ -11,17 +16,26 @@ function Start-Timer {
     )
 
     $Seconds += 60 * $Minutes
+    $blank = " " * 20
 
     while ($Seconds -gt 0) {
-        Write-Host "`rSeconds left: $Seconds                " -NoNewLine
+        Write-Host "`rSeconds left: $Seconds$blank" -NoNewLine
         Start-Sleep -Seconds 1
         $Seconds--
     }
 
-    Write-Host "`rSeconds left: $Seconds                " -NoNewLine
+    Write-Host "`rSeconds left: $Seconds$blank" -NoNewLine
 
     if ($Bell) {
-        Write-Output "`a"
+        # (karlr 2023_09_22_200523) The bell escape sequence does not seem
+        # to work, at least not as well as I want it to
+        #
+        # link
+        # - url: https://superuser.com/questions/1623573/powershell-how-to-play-different-system-sounds
+        [System.Console]::Beep(1000,300)
+
+        # # old (karlr 2023_09_22_200426)
+        # Write-Output "`a"
     }
 }
 
