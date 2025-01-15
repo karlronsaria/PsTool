d4f705f27ab019e9060b2871e4051f545040214e
karlr <andrew.mikedaniels@gmail.com>
2025-01-14 21:21:52 -0800

    feat(Registry): New-ItemProperty
    feat(Registry): New-ItemProperty with PropertyType ArgumentCompleter

--- script/Registry.ps1
+++ script/Registry.ps1
@@ -156,3 +156,138 @@ function Rename-ItemProperty {
     }
 }
 
+function New-ItemProperty {
+[CmdletBinding(DefaultParameterSetName='Path', SupportsShouldProcess=$true, ConfirmImpact='Medium', HelpUri='https://go.microsoft.com/fwlink/?LinkID=2096813')]
+param(
+    [Parameter(ParameterSetName='Path', Mandatory=$true, Position=0)]
+    [string[]]
+    ${Path},
+
+    [Parameter(ParameterSetName='LiteralPath', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+    [Alias('PSPath','LP')]
+    [string[]]
+    ${LiteralPath},
+
+    [Parameter(Mandatory=$true, Position=1, ValueFromPipelineByPropertyName=$true)]
+    [Alias('PSProperty')]
+    [string]
+    ${Name},
+
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [Alias('Type')]
+    [ArgumentCompleter({
+        Param($A, $B, $C)
+
+        $names = [enum]::GetNames([Microsoft.Win32.RegistryValueKind])
+        $suggests = $names | where { $_ -like "$C*" }
+
+        if (@($suggests).Count -eq 0) {
+            $name
+        }
+        else {
+            $suggests
+        }
+    })]
+    [string]
+    ${PropertyType},
+
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [System.Object]
+    ${Value},
+
+    [switch]
+    ${Force},
+
+    [string]
+    ${Filter},
+
+    [string[]]
+    ${Include},
+
+    [string[]]
+    ${Exclude},
+
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [pscredential]
+    [System.Management.Automation.CredentialAttribute()]
+    ${Credential})
+
+
+dynamicparam
+{
+    try {
+        $targetCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\New-ItemProperty', [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters)
+        $dynamicParams = @($targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic })
+        if ($dynamicParams.Length -gt 0)
+        {
+            $paramDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
+            foreach ($param in $dynamicParams)
+            {
+                $param = $param.Value
+
+                if(-not $MyInvocation.MyCommand.Parameters.ContainsKey($param.Name))
+                {
+                    $dynParam = [Management.Automation.RuntimeDefinedParameter]::new($param.Name, $param.ParameterType, $param.Attributes)
+                    $paramDictionary.Add($param.Name, $dynParam)
+                }
+            }
+
+            return $paramDictionary
+        }
+    } catch {
+        throw
+    }
+}
+
+begin
+{
+    try {
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+        {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+
+        $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\New-ItemProperty', [System.Management.Automation.CommandTypes]::Cmdlet)
+        $scriptCmd = {& $wrappedCmd @PSBoundParameters }
+
+        $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+        $steppablePipeline.Begin($PSCmdlet)
+    } catch {
+        throw
+    }
+}
+
+process
+{
+    try {
+        $steppablePipeline.Process($_)
+    } catch {
+        throw
+    }
+}
+
+end
+{
+    try {
+        $steppablePipeline.End()
+    } catch {
+        throw
+    }
+}
+
+clean
+{
+    if ($null -ne $steppablePipeline) {
+        $steppablePipeline.Clean()
+    }
+}
+<#
+
+.ForwardHelpTargetName Microsoft.PowerShell.Management\New-ItemProperty
+.ForwardHelpCategory Cmdlet
+
+#>
+
+}
+
