3ea9739a97a1781fff1d135ccabfda235ef4d103
karlr <andrew.mikedaniels@gmail.com>
2026-04-26 20:59:16 -0700

    feat(demand): GoogleChrome
    refactor: move Windows to Workaround

--- demand/Package.ps1
+++ demand/Package.ps1
@@ -15,8 +15,8 @@ function Get-PackageMoniker {
                 -PercentComplete 0
 
             $path = [PsCustomObject]@{
-                Choco = "C:\shortcut\dos\backup\choco\packages.config"
-                Winget = "C:\shortcut\dos\backup\winget\package.json"
+                Choco = "C:/shortcut/dos/backup/choco/packages.config"
+                Winget = "C:/shortcut/dos/backup/winget/package.json"
             }
 
             $cargo = cargo install --list |
@@ -127,8 +127,8 @@ function Get-PackageMoniker {
         -PercentComplete 0
 
     $path = [PsCustomObject]@{
-        Choco = "C:\shortcut\dos\backup\choco\packages.config"
-        Winget = "C:\shortcut\dos\backup\winget\package.json"
+        Choco = "C:/shortcut/dos/backup/choco/packages.config"
+        Winget = "C:/shortcut/dos/backup/winget/package.json"
     }
 
     $cargo = cargo install --list |
@@ -306,3 +306,105 @@ function Install-Chocolatey {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($url))
 }
+
+function Show-ChocoInstalledPackage {
+    Param(
+        [switch]
+        $GetPath
+    )
+
+    $path = "C:/ProgramData/chocolatey/logs/chocolatey.log"
+
+    if ($GetPath) {
+        return $path
+    }
+
+    $path |
+        Get-Item |
+        Get-Content |
+        Select-String -Pattern "Successfully installed"
+}
+
+<#
+.DESCRIPTION
+Tags: windows, installed, programs
+#>
+function Get-LocalPackage {
+    [OutputType([PsCustomObject[]])]
+    Param(
+        [String]
+        $DisplayName,
+
+        [String]
+        $DisplayVersion,
+
+        [String]
+        $Publisher,
+
+        [DateTime]
+        $InstallByDate,
+
+        [Switch]
+        $Full
+    )
+
+    $pattern =
+    "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+    if (-not $Full) {
+        $packages = Get-ItemProperty $pattern |
+            Select-Object `
+                DisplayName,
+                DisplayVersion,
+                Publisher,
+                InstallDate
+    }
+
+    if ($DisplayName) {
+        $packages = $packages | Where-Object {
+            $_.DisplayName -like $DisplayName
+        }
+    }
+
+    if ($DisplayVersion) {
+        $packages = $packages | Where-Object {
+            $_.DisplayVersion -like $DisplayVersion
+        }
+    }
+
+    if ($Publisher) {
+        $packages = $packages | Where-Object {
+            $_.Publisher -like $Publisher
+        }
+    }
+
+    $packages = $packages |
+        Where-Object { $_.DisplayName } |
+        ForEach-Object {
+            [PsCustomObject]@{
+                DisplayName = $_.DisplayName
+                DisplayVersion = $_.DisplayVersion
+                Publisher = $_.Publisher
+                InstallDate = if ($_.InstallDate) {
+                    [DateTime]::ParseExact(
+                        $_.InstallDate,
+                        "yyyyMMdd",
+                        $null
+                    )
+                }
+                else {
+                    $null
+                }
+            }
+        }
+
+    if ($InstallByDate) {
+        $packages = $packages | Where-Object {
+            $null -ne $_.InstallDate -and `
+            $_.InstallDate -le $InstallByDate
+        }
+    }
+
+    return $packages
+}
+
