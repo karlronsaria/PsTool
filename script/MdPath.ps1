7133eea29e600d1cba669a44fd275d5dab995790
karlr <andrew.mikedaniels@gmail.com>
2026-09-02 03:42:58 -0700

    refactor(HowtoTree): replace with MdPath
    refactor(HowtoTree): replace with MdPath and make it extensible

--- script/MdPath.ps1
+++ script/MdPath.ps1
@@ -1,9 +1,37 @@
-function Get-HowtoTree {
+class BranchDefinition {
+    [scriptblock] $Find = { $true }
+    [scriptblock] $Post = { $args[0] }
+}
+
+function Find-MdPath {
     [CmdletBinding(DefaultParameterSetName = 'All')]
     Param(
         [Parameter(ValueFromPipeline = $true)]
         [pscustomobject]
         $InputObject,
+        
+        [ValidateSet('code', 'link')]
+        [Parameter(
+            ParameterSetName = 'All',
+            Position = 0
+        )]
+        [Parameter(
+            ParameterSetName = 'ByTreePath',
+            Position = 0
+        )]
+        [string]
+        $Type,
+        
+        [Parameter(
+            ParameterSetName = 'AllCustom',
+            Position = 0
+        )]
+        [Parameter(
+            ParameterSetName = 'ByTreePathCustom',
+            Position = 0
+        )]
+        [BranchDefinition]
+        $CustomType,
 
         [ArgumentCompleter({
             [OutputType([System.Management.Automation.CompletionResult])]
@@ -17,7 +45,7 @@ function Get-HowtoTree {
             
             $CompletionResults = [System.Collections.Generic.List[System.Management.Automation.CompletionResult]]::new()
             
-            Get-HowtoTree |
+            Find-MdPath @FakeBoundParameters |
                 ForEach-Object TreePath |
                 Where-Object { $_ -like "*$WordToComplete*" } |
                 ForEach-Object { "`"$_`"" } |
@@ -28,22 +56,33 @@ function Get-HowtoTree {
         })]
         [Parameter(
             ParameterSetName = 'ByTreePath',
-            Position = 0
+            Position = 1
+        )]
+        [Parameter(
+            ParameterSetName = 'ByTreePathCustom',
+            Position = 1
         )]
         [string[]]
         $TreePath,
 
         [Parameter(ParameterSetName = 'ByTreePath')]
+        [Parameter(ParameterSetName = 'ByTreePathCustom')]
         [ValidateSet('And', 'Or')]
         [string]
-        $Mode = 'And'
+        $Mode = 'Or',
+
+        [switch]
+        $Recurse
     )
 
     Begin {
         function Get-Forest {
             Param(
                 [Parameter(ValueFromPipeline = $true)]
-                $InputString
+                $InputString,
+
+                [scriptblock]
+                $Where
             )
             
             Begin {
@@ -59,21 +98,21 @@ function Get-HowtoTree {
                 Get-MarkdownTree -AsMarkdown |
                 ForEach-Object {
                     $tree = $_
-                    $tree.PathOfAll({ $args[0] -is [MarkdownTree.Parse.CodeBlock] })
+                    $tree.PathOfAll($Where)
                 } |
-                Where-Object { $_.Count -gt 0 } |
+                Where-Object { $_ -and $_.Count -gt 0 } |
                 ForEach-Object {
                     $path = $_
                     $branch = $tree
                     $index = 0
                     $list = @()
 
-                    while ($branch.Children.Count -gt 0) {
+                    while ($branch.Children.Count -gt 0 -and $index -lt $path.Count) {
                         $list += $branch
                         $branch = $branch.Children[$path[$index++]]
                     }
 
-                    if ($list.Count -gt 0 -and $branch -is [MarkdownTree.Parse.CodeBlock]) {
+                    if ($list.Count -gt 0) {
                         $lineAbove = $list[-1].Children[$path[$index - 1] - 1]
 
                         if ($lineAbove.LineType -eq "Paragraph") {
@@ -82,7 +121,7 @@ function Get-HowtoTree {
                     }
 
                     [pscustomobject]@{
-                        CodeBlock = $branch
+                        Needle = $branch
                         TreePath = $list.Name -join ": "
                     }
                 }
@@ -94,28 +133,63 @@ function Get-HowtoTree {
             Get-Content |
             ConvertFrom-Json |
             ForEach-Object Notebooks
-            
+
+        $types = @{
+            '' = [BranchDefinition]::new()
+            'code' = [BranchDefinition]@{
+                Find = { $args[0] -is [MarkdownTree.Parse.CodeBlock] }
+                Post = { $args[0] }
+            }
+            'link' = [BranchDefinition]@{
+                Find = { $args[0].Content -and $args[0].Content.WhereAll({ $args[0].TokenType -eq 'Hyperlink' }).Count -gt 0 }
+                Post = { $args[0].Content[0].Content }
+            }
+        }
+
+        $files = @()
         $forest = @()
     }
     
     Process {
-        $forest += @($InputObject | Where-Object { $_ })
+        if ($InputObject -is [System.IO.FileSystemInfo]) {
+            $files += @($InputObject | Where-Object { $_ })
+        }
+        elseif ($InputObject -is [pscustomobject]) {
+            $forest += @($InputObject | Where-Object { $_ })
+        }
+        elseif ($InputObject -is [string]) {
+            $files += @($InputObject | Get-ChildDocumentItem -Recurse:$Recurse)
+        }
     }
 
     End {
+        $definition = 
+            if ($CustomType) {
+                $CustomType
+            }
+            else {
+                $types[$Type]
+            }
+        
+        if ($files.Count -eq 0 -and $forest.Count -eq 0) {
+            $files = $notebooks |
+                Get-ChildDocumentItem `
+                    -Recurse:$Recurse
+        }
+        
         if ($forest.Count -eq 0) {
-            $forest = $notebooks |
-                Get-ChildDocumentItem |
+            $forest = $files |
                 ForEach-Object {
                     $file = $_
 
                     $file |
                     Get-Content |
-                    Get-Forest |
+                    Get-Forest `
+                        -Where:$definition.Find |
                     ForEach-Object {
                         [pscustomobject]@{
                             TreePath = $_.TreePath
-                            CodeBlock = $_.CodeBlock
+                            Needle = & $definition.Post $_.Needle
                             FilePath = $file
                         }
                     }
@@ -151,8 +225,8 @@ function Get-HowtoTree {
 }
 
 New-Alias `
-    -Name 'howto' `
-    -Value 'Get-HowtoTree' `
+    -Name 'pathto' `
+    -Value 'Find-MdPath' `
     -Scope Global `
     -Option ReadOnly `
     -Force
